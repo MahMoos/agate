683807048e519bb514e2d6571108aae63bc79650
MahMoos <mahmoos313@gmail.com>
2024-07-27 02:10:44 +0300

    fix: remove mcq games tab from subject_page

--- lib/features/explore/presentation/pages/subject_page.dart
+++ lib/features/explore/presentation/pages/subject_page.dart
@@ -10,50 +10,29 @@ class SubjectPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Consumer(
-        builder: (context, ref, child) {
-          final subject = ref.watch(subjectProvider(id));
-          return subject.when(
-            data: (subject) => Scaffold(
-              appBar: AppBar(
-                title: Text(subject.name),
-                bottom: TabBar(
-                  tabs: [
-                    Tab(child: Text(context.strings.books)),
-                    Tab(child: Text(context.strings.mcqs)),
-                  ],
-                ),
-              ),
-              body: TabBarView(
-                children: [
-                  EndlessAnimatedListView(
-                    provider: booksProvider(
-                      BooksParams(subjectId: subject.id),
-                    ),
-                    itemBuilder: (context, book) => BookTile(
-                      book: book,
-                    ).paddingSymmetric(horizontal: 12, vertical: 4),
-                  ),
-                  EndlessAnimatedListView(
-                    provider: mcqGamesProvider(
-                      McqGamesParams(subjectId: subject.id),
-                    ),
-                    itemBuilder: (context, mcqGame) => McqView(
-                      mcq: mcqGame,
-                    ).paddingSymmetric(horizontal: 12, vertical: 4),
-                  ),
-                ],
-              ),
+    return Consumer(
+      builder: (context, ref, child) {
+        final subject = ref.watch(subjectProvider(id));
+        return subject.when(
+          data: (subject) => Scaffold(
+            appBar: AppBar(
+              title: Text(subject.name),
             ),
-            error: (err, stack) => StatusView.anErrorOccurred(
-              action: () => ref.refresh(subjectProvider(id).future),
+            body: EndlessAnimatedListView(
+              provider: booksProvider(
+                BooksParams(subjectId: subject.id),
+              ),
+              itemBuilder: (context, book) => BookTile(
+                book: book,
+              ).paddingSymmetric(horizontal: 12, vertical: 4),
             ),
-            loading: () => const Center(child: CircularProgressIndicator()),
-          );
-        },
-      ),
+          ),
+          error: (err, stack) => StatusView.anErrorOccurred(
+            action: () => ref.refresh(subjectProvider(id).future),
+          ),
+          loading: () => const Center(child: CircularProgressIndicator()),
+        );
+      },
     );
   }
 }
