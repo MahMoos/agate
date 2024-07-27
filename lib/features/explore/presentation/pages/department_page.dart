e3e089a5953b8dd0c7d7fba4cf9c06cb04f917c5
MahMoos <mahmoos313@gmail.com>
2024-07-27 11:32:53 +0300

    feat: show sub departments on navigation to new department_page.dart

--- lib/features/explore/presentation/pages/department_page.dart
+++ lib/features/explore/presentation/pages/department_page.dart
@@ -28,60 +28,93 @@ class _DepartmentPageState extends State<DepartmentPage>
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Consumer(
-        builder: (context, ref, child) {
-          final department = ref.watch(
-            departmentProvider(widget.id),
-          );
-          return department.when(
-            data: (department) => Scaffold(
-              appBar: AppBar(
-                title: Text(department.name),
-                bottom: TabBar(
-                  tabs: [
-                    Tab(child: Text(context.strings.subjects)),
-                    Tab(child: Text(context.strings.courses)),
-                  ],
+    return Consumer(
+      builder: (context, ref, child) {
+        final department = ref.watch(
+          departmentProvider(widget.id),
+        );
+        return department.when(
+          data: (department) {
+            if (department.subDepartments != null &&
+                department.subDepartments!.isNotEmpty) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text(department.name),
                 ),
-              ),
-              body: TabBarView(
-                children: [
-                  EndlessAnimatedListView(
-                    provider:
-                        subjectsProvider(SubjectsParams(divisionId: widget.id)),
-                    itemBuilder: (context, subject) => SizedBox(
+                body: ListView.builder(
+                  itemCount: department.subDepartments!.length,
+                  itemBuilder: (
+                    BuildContext context,
+                    int index,
+                  ) {
+                    return SizedBox(
                       height: 178,
                       child: ShortcutCard(
-                        title: subject.name,
-                        imageUrl: subject.imageUrl,
+                        title: department.subDepartments![index].name,
+                        imageUrl: department.subDepartments![index].logoUrl,
                         imageFit: BoxFit.contain,
                         imageAlignment: context.isRTL
                             ? Alignment.centerLeft
                             : Alignment.centerRight,
-                        onTap: () => context.push('/subjects/${subject.id}'),
+                        onTap: () => context.push(
+                          '/departments/${department.subDepartments![index].id}',
+                        ),
+                      ).paddingSymmetric(horizontal: 12, vertical: 4),
+                    );
+                  },
+                ),
+              );
+            }
+            return DefaultTabController(
+              length: 2,
+              child: Scaffold(
+                appBar: AppBar(
+                  title: Text(department.name),
+                  bottom: TabBar(
+                    tabs: [
+                      Tab(child: Text(context.strings.subjects)),
+                      Tab(child: Text(context.strings.courses)),
+                    ],
+                  ),
+                ),
+                body: TabBarView(
+                  children: [
+                    EndlessAnimatedListView(
+                      provider: subjectsProvider(
+                        SubjectsParams(divisionId: widget.id),
+                      ),
+                      itemBuilder: (context, subject) => SizedBox(
+                        height: 178,
+                        child: ShortcutCard(
+                          title: subject.name,
+                          imageUrl: subject.imageUrl,
+                          imageFit: BoxFit.contain,
+                          imageAlignment: context.isRTL
+                              ? Alignment.centerLeft
+                              : Alignment.centerRight,
+                          onTap: () => context.push('/subjects/${subject.id}'),
+                        ).paddingSymmetric(horizontal: 12, vertical: 4),
+                      ),
+                    ),
+                    EndlessAnimatedListView(
+                      provider: coursesProvider(
+                        CoursesParams(divisionIds: [department.id]),
+                      ),
+                      itemBuilder: (context, course) => CourseCard(
+                        details: course,
                       ).paddingSymmetric(horizontal: 12, vertical: 4),
                     ),
-                  ),
-                  EndlessAnimatedListView(
-                    provider: coursesProvider(
-                      CoursesParams(divisionIds: [department.id]),
-                    ),
-                    itemBuilder: (context, course) => CourseCard(
-                      details: course,
-                    ).paddingSymmetric(horizontal: 12, vertical: 4),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            error: (err, stack) => StatusView.anErrorOccurred(
-              action: () => ref.refresh(departmentProvider(widget.id).future),
-            ),
-            loading: () => const Center(child: CircularProgressIndicator()),
-          );
-        },
-      ),
+            );
+          },
+          error: (err, stack) => StatusView.anErrorOccurred(
+            action: () => ref.refresh(departmentProvider(widget.id).future),
+          ),
+          loading: () => const Center(child: CircularProgressIndicator()),
+        );
+      },
     );
   }
 
